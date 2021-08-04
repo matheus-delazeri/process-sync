aa5be6f59e9d3045b739c829911c5b29e8e2298f
matheus <matheusdelazeris@gmail.com>
2021-08-04 17:08:06 -0300

    Show trace function

--- main.c
+++ main.c
@@ -5,18 +5,34 @@
 #include <unistd.h>
 
 sem_t main_empty,sec_empty;
-sem_t sem_main;
+sem_t sem_main, sem_sec;
 sem_t impress;
 pthread_t *main_buffer, *sec_buffer;
 int main_index = 0, sec_index = 0, MAX;
 
-void show_trace(){
-    printf("Threads in main buffer\n");
-    printf("----------------------\n");
+void show_trace(int file_type, long int id, int imp_time){
+    printf("\n---------------------------------------\n");
+    printf("\nArquivo sendo impresso: [%ld]\n", id);
+    switch (file_type){
+    case 0:
+        printf("Tipo do arquivo: administrativo\n");
+        break;
+    case 1:
+        printf("Tipo do arquivo: diverso\n");
+        break; 
+    }
+    printf("Tempo estimado: %ds\n", imp_time);
+    printf("\nArquivos no buffer principal\n");
+    printf("--------\n");
     for(int i=0; i<MAX; i++){
         printf("Slot %d [%ld] \n", i, main_buffer[i]);
     }
-    printf("----------------------\n");
+    printf("\nArquivos no buffer secundario\n");
+    printf("--------\n");
+    for(int i=0; i<MAX; i++){
+        printf("Slot %d [%ld] \n", i, sec_buffer[i]);
+    }
+    printf("\n---------------------------------------\n");
 }
 
 void *adm_thread(){
@@ -30,11 +46,13 @@ void *adm_thread(){
     sem_post(&sem_main);
 
     sem_wait(&impress);
-    sleep(1);
+    srand(time(NULL)); 
+    int imp_time = (rand() % 3)+1;
+    show_trace(0, id, imp_time);
+    sleep(imp_time);
     if(main_index!=0){
         main_index--;
     }
-    show_trace();
     for(int i=0; i<MAX-1; i++){
         main_buffer[i] = main_buffer[i+1];
         main_buffer[i+1] = 0;
@@ -46,13 +64,29 @@ void *adm_thread(){
 }
 
 void *div_thread(){
-    sec_index++;
-    sem_wait(&impress);
-
     pthread_t id = pthread_self();
+    sem_wait(&sem_sec);
+    sec_buffer[sec_index] = id;
+    int cur_index = sec_index;
+    if(sec_index!=MAX){
+        sec_index++;
+    }
+    sem_post(&sem_sec);
+
+    sem_wait(&impress);
+    srand(time(NULL)); 
+    int imp_time = (rand() % 3)+1;
+    show_trace(1, id, imp_time);
+    sleep(imp_time);
+    if(sec_index!=0){
+        sec_index--;
+    }
+    for(int i=0; i<MAX-1; i++){
+        sec_buffer[i] = sec_buffer[i+1];
+        sec_buffer[i+1] = 0;
+    }
 
     sem_post(&impress);
-    sec_index--;
     sem_post(&sec_empty);
     return NULL;
 }
@@ -61,7 +95,6 @@ int main(int argc, char **argv){
     int QTD_ADM, QTD_DIVERSOS, thread_count = 0;
     int adm_count = 0, div_count = 0;
     pthread_t *all_threads;
-    srand(time(NULL)); 
     if (argc == 4 && strtol(argv[1], NULL, 10) != 0) {
         MAX = abs(strtol(argv[1], NULL, 10));
         QTD_ADM = abs(strtol(argv[2], NULL, 10));
@@ -79,6 +112,7 @@ int main(int argc, char **argv){
     sem_init(&main_empty, 0, MAX);
     sem_init(&sem_main, 0, 1);
     sem_init(&sec_empty, 0, MAX);
+    sem_init(&sem_sec, 0, 1);
     sem_init(&impress, 0, 1);
     main_buffer = (pthread_t*)malloc(MAX * sizeof(pthread_t));
     sec_buffer = (pthread_t*)malloc(MAX * sizeof(pthread_t));
@@ -103,6 +137,7 @@ int main(int argc, char **argv){
     sem_destroy(&main_empty);
     sem_destroy(&sem_main);
     sem_destroy(&sec_empty);
+    sem_destroy(&sem_sec);
     sem_destroy(&impress);
 
     free(main_buffer);
