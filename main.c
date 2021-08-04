2f08ce2c83a26a3685d96da89f0f85ca1fc58ae6
matheus <matheusdelazeris@gmail.com>
2021-08-03 21:29:11 -0300

    Parameters validation

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <stdlib.h>
 
-int main(){
+int main(int argc, char **argv){
+    int MAX, QTD_ADM, QTD_DIVERSOS;
+    double seq_time, chunk_time, esp_time;
+    if (argc == 4 && strtol(argv[1], NULL, 10) != 0) {
+        MAX = abs(strtol(argv[1], NULL, 10));
+        QTD_ADM = abs(strtol(argv[2], NULL, 10));
+        QTD_DIVERSOS = abs(strtol(argv[3], NULL, 10));
+    } else if(argc > 1 && strtol(argv[1], NULL, 10)==0) {
+        printf("O armazenamento (MAX) deve ser maior que 0.\n");
+        exit(0);
+    }
+    else {
+        printf("É necessário passar a capacidade de armazenamento (MAX), o numero de arquivos administrativos (QTD_ADM) e o numero de arquivos diversos (QTD_DIVERSOS) por parametro.\n");
+        printf("Ex.: ./<file-name> 100 4 4\n");
+        exit(0);
+    }
 
 }
