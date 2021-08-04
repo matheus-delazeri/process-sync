d2a51241e4ed1163625f45cf873d62521d91d462
matheus <matheusdelazeris@gmail.com>
2021-08-03 22:41:10 -0300

    ADM thread generation

--- main.c
+++ main.c
@@ -3,9 +3,27 @@
 #include <pthread.h>
 #include <stdlib.h>
 
+typedef struct {
+    int *gen_time;
+    int *imp_time;
+} file_struct;
+
+void *impress(void *file){
+    file_struct *actual_file = file;
+    pthread_t id = pthread_self();
+    int gen_time = *(actual_file->gen_time);
+    int imp_time = *(actual_file->imp_time);
+    int *return_args = malloc(sizeof (int));
+    return_args[0] = id;
+
+    free(actual_file);
+    return return_args;
+}
+
 int main(int argc, char **argv){
-    int MAX, QTD_ADM, QTD_DIVERSOS;
-    double seq_time, chunk_time, esp_time;
+    int i, imp_time, gen_time, MAX, QTD_ADM, QTD_DIVERSOS;
+    pthread_t *adm_vect, *div_vect;
+    srand(time(NULL)); 
     if (argc == 4 && strtol(argv[1], NULL, 10) != 0) {
         MAX = abs(strtol(argv[1], NULL, 10));
         QTD_ADM = abs(strtol(argv[2], NULL, 10));
@@ -20,4 +38,26 @@ int main(int argc, char **argv){
         exit(0);
     }
 
+    adm_vect = (pthread_t*)malloc(QTD_ADM * sizeof(pthread_t));
+    for(i=0; i<QTD_ADM; i++){
+        gen_time = rand()%6;
+        imp_time = rand()%6;
+        file_struct *file = malloc(sizeof *file);
+        file->gen_time = &gen_time;
+        file->imp_time = &imp_time;
+        if(pthread_create(&adm_vect[i], NULL, impress, file)){
+            free(file);
+        }
+    }
+    for(i=0; i<QTD_ADM; i++){
+        void *status = NULL;
+        pthread_join(adm_vect[i], &status);
+        printf("Id: %d\n", ((int*)status)[0]); 
+    }
+
+    div_vect = (pthread_t*)malloc(QTD_DIVERSOS * sizeof(pthread_t));
+
+
+    free(adm_vect);
+    free(div_vect);
 }
