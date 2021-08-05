140d5a9c3a26daa68d992ce426702ed2a1114f33
matheus <matheusdelazeris@gmail.com>
2021-08-04 22:01:09 -0300

    Final version

--- main.c
+++ main.c
@@ -4,35 +4,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-sem_t main_empty,sec_empty;
+sem_t main_empty, sec_empty;
 sem_t sem_main, sem_sec;
 sem_t impress_adm, impress_div;
 pthread_t *main_buffer, *sec_buffer;
 int main_index = 0, sec_index = 0, MAX, exec_time = 0, adm_ver = 0;
 
 void show_trace(int file_type, long int id, int imp_time){
-    printf("\n---------------------------------------\n");
-    printf("\nArquivo sendo impresso: [%ld]\n", id);
+    printf("\n\t-------------------------------------------\n");
+    printf("\n\tArquivo sendo impresso: [%ld]\n", id);
     switch (file_type){
     case 0:
-        printf("Tipo do arquivo: administrativo\n");
+        printf("\tTipo do arquivo: administrativo\n");
         break;
     case 1:
-        printf("Tipo do arquivo: diverso\n");
+        printf("\tTipo do arquivo: diverso\n");
         break; 
     }
-    printf("Tempo estimado: %ds\n", imp_time);
-    printf("\nArquivos no buffer principal\n");
-    printf("--------\n");
+    printf("\tTempo estimado: %ds\n", imp_time);
+    printf("\n\tArquivos no buffer principal\n");
+    printf("\t-----------\n");
     for(int i=0; i<MAX; i++){
-        printf("Slot %d - [%ld] \n", i, main_buffer[i]);
+        printf("\tSlot %d - [%ld] \n", i, main_buffer[i]);
     }
-    printf("\nArquivos no buffer secundario\n");
-    printf("--------\n");
+    printf("\n\tArquivos no buffer secundario\n");
+    printf("\t-----------\n");
     for(int i=0; i<MAX; i++){
-        printf("Slot %d - [%ld] \n", i, sec_buffer[i]);
+        printf("\tSlot %d - [%ld] \n", i, sec_buffer[i]);
     }
-    printf("\n---------------------------------------\n");
 }
 int buffer_empty(int buffer){
     int ver = 1;
@@ -62,7 +61,6 @@ void *adm_thread(){
     sem_wait(&main_empty);
     sem_wait(&sem_main);
     main_buffer[main_index] = id;
-    int cur_index = main_index;
     if(main_index!=MAX){
         main_index++;
     }
@@ -78,19 +76,22 @@ void *adm_thread(){
     if(main_index!=0){
         main_index--;
     }
-    for(int i=0; i<MAX-1; i++){
-        main_buffer[i] = main_buffer[i+1];
-        main_buffer[i+1] = 0;
+    if(MAX!=1){
+        for(int i=0; i<MAX-1; i++){
+                main_buffer[i] = main_buffer[i+1];
+                main_buffer[i+1] = 0;
+        }
     }
 
+    printf("\n\tO arquivo [%ld] foi impresso com sucesso!\n", id);
+    printf("\n\t-------------------------------------------\n");
     sem_post(&main_empty);
-    if(adm_ver==2 && buffer_empty(1)==0){
+    if((adm_ver==2 && buffer_empty(1)==0)||buffer_empty(0)==1){
         sem_post(&impress_div);
     }else{
         sem_post(&impress_adm);
     }
 
-    return NULL;
 }
 
 void *div_thread(){
@@ -98,7 +99,6 @@ void *div_thread(){
     sem_wait(&sec_empty);
     sem_wait(&sem_sec);
     sec_buffer[sec_index] = id;
-    int cur_index = sec_index;
     if(sec_index!=MAX){
         sec_index++;
     }
@@ -113,24 +113,28 @@ void *div_thread(){
     if(sec_index!=0){
         sec_index--;
     }
-    for(int i=0; i<MAX-1; i++){
-        sec_buffer[i] = sec_buffer[i+1];
-        sec_buffer[i+1] = 0;
+    if(MAX!=1){
+        for(int i=0; i<MAX-1; i++){
+            sec_buffer[i] = sec_buffer[i+1];
+            sec_buffer[i+1] = 0;
+        }
     }
     adm_ver = 0;
 
+    printf("\n\tO arquivo [%ld] foi impresso com sucesso!\n", id);
+    printf("\n\t-------------------------------------------\n");
     sem_post(&sec_empty);
     if(buffer_empty(0)==0){
         sem_post(&impress_adm);
     }else{
         sem_post(&impress_div);
     }
-    return NULL;
+
 }
 
 int main(int argc, char **argv){
     int QTD_ADM, QTD_DIVERSOS, thread_count = 0;
-    int adm_count = 0, div_count = 0;
+    int adm_count = 0, div_count = 0, imp_div_value;
     pthread_t *all_threads;
     if (argc == 4 && strtol(argv[1], NULL, 10) != 0) {
         MAX = abs(strtol(argv[1], NULL, 10));
@@ -145,13 +149,14 @@ int main(int argc, char **argv){
         printf("Ex.: ./<file-name> 100 4 4\n");
         exit(0);
     }
+    imp_div_value = QTD_ADM!=0 ? 0 : 1;
 
     sem_init(&main_empty, 0, MAX);
     sem_init(&sem_main, 0, 1);
     sem_init(&sec_empty, 0, MAX);
     sem_init(&sem_sec, 0, 1);
     sem_init(&impress_adm, 0, 1);
-    sem_init(&impress_div, 0, 0);
+    sem_init(&impress_div, 0, imp_div_value);
     main_buffer = (pthread_t*)malloc(MAX * sizeof(pthread_t));
     sec_buffer = (pthread_t*)malloc(MAX * sizeof(pthread_t));
     all_threads = (pthread_t*)malloc((QTD_ADM+QTD_DIVERSOS) * sizeof(pthread_t));
@@ -169,12 +174,22 @@ int main(int argc, char **argv){
     for(int i=0; i<(QTD_ADM+QTD_DIVERSOS); i++){
         pthread_join(all_threads[i], NULL);
     }
-    printf("\n---------------------------------------\n");
-    printf("Arquivos administrativos impressos: [%d]\n", QTD_ADM);
-    printf("Arquivos diversos impressos: [%d]\n", QTD_DIVERSOS);
-    printf("Total: [%d]\n", QTD_ADM+QTD_DIVERSOS);
-    printf("Tempo de execucao: %ds\n", exec_time);
-    printf("---------------------------------------\n");
+    printf("\n\tArquivos no buffer principal\n");
+    printf("\t-----------\n");
+    for(int i=0; i<MAX; i++){
+        printf("\tSlot %d - [%ld] \n", i, main_buffer[i]);
+    }
+    printf("\n\tArquivos no buffer secundario\n");
+    printf("\t-----------\n");
+    for(int i=0; i<MAX; i++){
+        printf("\tSlot %d - [%ld] \n", i, sec_buffer[i]);
+    }
+    printf("\n\t-------------------------------------------\n");
+    printf("\tArquivos administrativos impressos: [%d]\n", QTD_ADM);
+    printf("\tArquivos diversos impressos: [%d]\n", QTD_DIVERSOS);
+    printf("\tTotal: [%d]\n", QTD_ADM+QTD_DIVERSOS);
+    printf("\tTempo de execucao: %ds\n", exec_time);
+    printf("\t-------------------------------------------\n");
 
     sem_destroy(&main_empty);
     sem_destroy(&sem_main);
